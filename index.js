bdd0fad74f4a78a2f8942e469970899660ada2ca
huweixuan <weixuan.hu@dianping.com>
2014-05-09 17:07:16 +0800

    add some test; #1

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ var fs = require('fs');
 var path = require('path');
 var through = require('through');
 var stat = fs.stat;
+var allNum = 0; 
     
 //Copy all files in the directory, including subdirectories
-var copy = function ( src, dst, callback ) {
+var copy = function ( name, src, dst, callback ) {
   fs.readdir( src, function ( err, paths ) {
     if( err ) {
       throw err;
     }
 
+    allNum += paths.length;
+
     paths.forEach( function ( path ) {
       var _src = src + '/' + path;
-      var _dst = path == 'test/spec/name.js' 
-        ? dest + '/test/spec/' + pkg.name + '.js' 
-        : dst + '/' + path;
+      var _dst = path == 'name.js' ? dst + '/' + name + '.js' : dst + '/' + path;
       var readable;
       var writable;        
 
@@ -25,17 +26,30 @@ var copy = function ( src, dst, callback ) {
           throw err;
         }
 
+        var fileNum = 0;
+
         if( st.isFile() ) {
+          fileNum++;
           readable = fs.createReadStream( _src );
           writable = fs.createWriteStream( _dst );
-          writable.on('finish', function () {
-		    callback();
+
+          //When all files were copied, run the 'callback' function.
+          writable.on( 'finish', function () {
+          	fileNum--;
+          	allNum--;
+          	if( fileNum === 0 ) {
+          	  allNum -= 1;
+          	}
+          	if( allNum === 0 ) {
+          	  callback();
+          	}
 		  });
           readable.pipe( through( function (buf) {
-            this.queue( buf.toString().replace( /\{%= name %\}/g, pkg.name ) );
+            this.queue( buf.toString().replace( /\{%= name %\}/g, name ) );
           })).pipe( writable );
         } else if( st.isDirectory() ) {
-          exists( _src, _dst, copy );
+          
+          exists( name, _src, _dst, callback );
         }
       });
     });
@@ -44,25 +58,25 @@ var copy = function ( src, dst, callback ) {
 
 // Before copying directories need to determine that the directory exists or not.
 // If the directory does not exist, need to create a directory
-var exists = function( src, dst, callback ) {
+var exists = function( name, src, dst, callback ) {
   fs.exists( dst, function ( exists ) {
     if( exists ) {
-      callback( src, dst );
+      copy( name, src, dst, callback );
     } else {
       fs.mkdir( dst, function () {
-        callback( src, dst );
+        copy( name, src, dst, callback );
       });
     }
   });
 };
 
-var Generator = function () {};
-
-Generator.DEFAULT = 'default';
+var Generator = function () {
+	this.DEFAULT = 'default';
+};
 
 Generator.prototype.generator = function (pkg, opts, callback) {
   var default_opts = {
-  	template: self.DEFAULT,
+  	template: this.DEFAULT,
     override: false 
   }
 
@@ -70,7 +84,7 @@ Generator.prototype.generator = function (pkg, opts, callback) {
   	throw new Error('Missing options \'cwd\'');
   }
 
-  exists( path.join('/templates', opts.template), opts.cwd, copy );  
+  exists( pkg.name, path.join('./templates', opts.template), opts.cwd, callback );  
 }
 
-module.exports = Generator;
+module.exports = new Generator();
--- test/cortex-scaffold-generator.js
+++ test/cortex-scaffold-generator.js
@@ -1,8 +1,32 @@
 'use strict';
 
 var expect = require('chai').expect;
-var cortex_scaffold_generator = require('../');
+var Generator = require('../');
+var fs = require('fs');
+var jf = require('jsonfile');
 
-describe("generator(pkg, opts, callback)", function () {
-  
+describe( 'Generator', function () {
+  it('should has property DEFAULT', function ( done ) {
+    expect(Generator.DEFAULT).to.be.a('string');
+    done();
+  });
+
+  describe( '#generator(pkg, opts, callback)', function () {
+    it('should copy all files in the directory, including subdirectories',
+  	  function ( done ) {
+        var pkg = jf.readFileSync('test/cortex.json');
+        var opts = { 
+          template: Generator.DEFAULT,
+          cwd     : 'test/expected',
+          override: true
+        };
+
+        Generator.generator( pkg, opts, function (err) {
+      	  var expected = fs.readFileSync('test/expected/index.js','utf-8');
+          expect(expected).not.to.be.null;
+          done();
+        });
+      }
+    );
+  });
 });
