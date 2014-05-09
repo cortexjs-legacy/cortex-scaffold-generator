bbe64fee6d5b2a5fbec6a9102c67816612336ed1
huweixuan <weixuan.hu@dianping.com>
2014-05-09 14:59:33 +0800

    complete the basic functions; #1

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 'use strict';
 
 var fs = require('fs');
+var path = require('path');
+var through = require('through');
 
-var generator = function (pkg, options, callback) {
-  
+var Generator = function () {};
+
+Generator.DEFAULT = 'default';
+
+Generator.prototype.generator = function (pkg, opts, callback) {
+  var self = this;
+  var default_opts = {
+  	template: self.DEFAULT,
+    override: false 
+  }
+  var pkg = pkg;
+
+  this.pkg = pkg;
+  this.opts = opts;
+
+  fs.createReadStream(path.join('/', self.opts.template))
+	.pipe(through(function (buf) {
+	  this.queue(buf.toString().repalce(/\{%= name %\}/g, self.pkg.name));
+	}))
+    .pipe(fs.createWriteStream(self.opts.cwd));
 }
 
 module.exports = generator;
