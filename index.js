3bed7d1546f68f9332fa33c61b4940a37223801a
huweixuan <weixuan.hu@dianping.com>
2014-05-12 14:30:17 +0800

    refactor; #4

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ var copy = function ( name, opts, done ) {
     async.each( paths, function ( path, callback ) {
       var _src = src + '/' + path;
       var _dst = path == 'name.js' ? dst + '/' + name + '.js' : dst + '/' + path;
-      var readable;
-      var writable;
 
       stat( _src, function ( err, st ) {
         if( err ){
@@ -28,15 +26,9 @@ var copy = function ( name, opts, done ) {
         }
         if( st.isFile() ) {
           fs.exists( _dst, function ( exists ) {
+            //override or not
             if( !exists || opts.override ) {
-              readable = fs.createReadStream( _src );
-              writable = fs.createWriteStream( _dst );
-              writable.on( 'finish', function () {
-                callback();
-              });
-              readable.pipe( through( function (buf) {
-                this.queue( buf.toString().replace( /\{%= name %\}/g, name ) );
-              })).pipe( writable );
+              doCopy( name, _src, _dst, callback );
             } else {
               callback();
             }
@@ -58,6 +50,17 @@ var copy = function ( name, opts, done ) {
   });
 };
 
+var doCopy = function ( name, src, dst, callback ) {
+  var readable = fs.createReadStream( src );
+  var writable = fs.createWriteStream( dst );
+  writable.on( 'finish', function () {
+    callback();
+  });
+  readable.pipe( through( function (buf) {
+    this.queue( buf.toString().replace( /\{%= name %\}/g, name ) );
+  })).pipe( writable );
+};
+
 // Before copying directories need to determine that the directory exists or not.
 // If the directory does not exist, need to create a directory
 var exists = function( name, opts, callback ) {
@@ -72,38 +75,38 @@ var exists = function( name, opts, callback ) {
   });
 };
 
-var generator = function (pkg, opts, callback) {
+var generator = function ( pkg, opts, callback ) {
 
   var default_opts = {
     template: 'default',
     override: false 
   }
 
-  if ( util.isNullOrUndefined(pkg) ) {
+  if ( util.isNullOrUndefined( pkg ) ) {
     var err = new Error( '\'pkg\' must be an object.' );
     return callback(err);
   }
 
-  if ( util.isNullOrUndefined(pkg.name) ) {
+  if ( util.isNullOrUndefined( pkg.name ) ) {
     var err = new Error( 'Missing \'pkg.name\'.' );
     return callback(err);
   }
 
-  if ( util.isNullOrUndefined(opts) ) {
+  if ( util.isNullOrUndefined( opts ) ) {
     var err = new Error( '\'opts\' must be an object.' );
     return callback(err);
   }
 
-  if ( util.isNullOrUndefined(opts.cwd) ) {
+  if ( util.isNullOrUndefined( opts.cwd ) ) {
     var err = new Error( 'Missing options \'cwd\'.' );
     return callback(err);
   }
 
-  if ( util.isNullOrUndefined(opts.template) ) {
+  if ( util.isNullOrUndefined( opts.template ) ) {
     opts.template = default_opts.template;
   }
 
-  if ( util.isNullOrUndefined(opts.override) ) {
+  if ( util.isNullOrUndefined( opts.override ) ) {
     opts.override = default_opts.override;
   }
 
