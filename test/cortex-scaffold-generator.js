5b362714c63cac16aa751fb4d70a4002cca3db4e
huweixuan <weixuan.hu@dianping.com>
2014-05-13 16:16:48 +0800

    add test for available licences and templates; #1

--- test/cortex-scaffold-generator.js
+++ test/cortex-scaffold-generator.js
@@ -6,45 +6,45 @@ var fs = require('fs');
 var jf = require('jsonfile');
 
 describe( 'generator(pkg, opts, callback)', function () {
-  it('should generator files with pkg and opts',
+  it( 'should generator files with pkg and opts',
     function ( done ) {
-      var pkg = jf.readFileSync('test/fixtures/cortex1.json');
+      var pkg = jf.readFileSync( 'test/fixtures/cortex1.json' );
       var opts = { 
         template: generator.AVAILABLE_TEMPLATES[0],
         cwd     : 'test/expected'
       };
 
-      generator( pkg, opts, function (err) {
-        var expected = fs.readFileSync('test/expected/index.js','utf-8');
-        expect(expected).not.to.be.null;
+      generator( pkg, opts, function ( err ) {
+        var expected = fs.readFileSync( 'test/expected/index.js','utf-8' );
+        expect( expected ).not.to.be.null;
         done();
       });
     }
   );
 
-  it('should override files when opts.override is true',
+  it( 'should override files when opts.override is true',
     function ( done ) {
-      var pkg = jf.readFileSync('test/fixtures/cortex2.json');
+      var pkg = jf.readFileSync( 'test/fixtures/cortex2.json' );
       var opts = { 
         template: generator.AVAILABLE_TEMPLATES[0],
         cwd     : 'test/expected',
         override: true
       };
 
-      generator( pkg, opts, function (err) {
-        var genPkg = jf.readFileSync('test/expected/package.json');
-        expect(genPkg.name).to.equal(pkg.name);
+      generator( pkg, opts, function ( err ) {
+        var genPkg = jf.readFileSync( 'test/expected/package.json' );
+        expect( genPkg.name ).to.equal( pkg.name );
         done();
       });
     }
   );
 
-  it('should only add cortex config in package.json when opts.override is false',
+  it( 'should only add cortex config in package.json when opts.override is false',
     function ( done ) {
-      var packageJsonFile = fs.readFileSync('test/fixtures/package.json');
-      fs.writeFileSync('test/expected/package.json', packageJsonFile);
-      
-      var pkg = jf.readFileSync('test/fixtures/cortex2.json');
+      var packageJsonFile = fs.readFileSync( 'test/fixtures/package.json' );
+      fs.writeFileSync( 'test/expected/package.json', packageJsonFile );
+
+      var pkg = jf.readFileSync( 'test/fixtures/cortex2.json' );
       var opts = {
         template: generator.AVAILABLE_TEMPLATES[0],
         cwd     : 'test/expected',
@@ -52,10 +52,24 @@ describe( 'generator(pkg, opts, callback)', function () {
       };
 
       generator( pkg, opts, function (err) {
-        var genPkg = jf.readFileSync('test/expected/package.json');
-        expect(genPkg.cortex).not.to.be.null;
+        var genPkg = jf.readFileSync( 'test/expected/package.json' );
+        expect( genPkg.cortex ).not.to.be.null;
         done();
       });
     }
   );
+
+  describe( '#availableLicences()', function () {
+    it( 'should return available licences array', function ( done ) {
+      expect( generator.availableLicences() ).to.be.a( 'array' );
+      done();
+    });
+  });
+
+  describe( '#AVAILABLE_TEMPLATES', function () {
+    it( 'should return available templates array', function ( done ) {
+      expect(generator.AVAILABLE_TEMPLATES ).to.be.a( 'array' );
+      done();
+    });
+  });
 });
