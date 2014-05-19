ef363d3235e06b2ca836fbbdcc0cd88fb4d7c229
kaelzhang <i@kael.me>
2014-05-19 19:48:30 +0800

    adds more hints for user; updates version

--- templates/default/test/{%=name%}.js
+++ templates/default/test/{%=name%}.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var {%= js_name %} = require('{%= main_in_test %}');
+
+// To know the usage of `assert`, see: http://nodejs.org/api/assert.html
 var assert = require('assert');
 
 describe("description", function(){
