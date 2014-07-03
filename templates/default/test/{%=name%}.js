d44e1982417c4e00265678586626bd0f42fc8a0a
kaelzhang <i@kael.me>
2014-07-03 23:46:29 +0800

    adds more comments; moves neurons/neuron/<version>/neuron.js -> neurons/neuron.js

--- templates/default/test/{%=name%}.js
+++ templates/default/test/{%=name%}.js
@@ -6,7 +6,7 @@ var {%= js_name %} = require('{%= main_in_test %}');
 var assert = require('assert');
 
 describe("description", function(){
-  it("should has a method `my_method`", function(){
-    assert.ok('my_method' in {%= js_name %});
+  it("should has a method `init`", function(){
+    assert.ok('init' in {%= js_name %});
   });
 });
