6acf1fcf82f4c706cda4908cba4dc8da95a0539c
kaelzhang <i@kael.me>
2014-07-04 15:21:36 +0800

    use a more easy case for hello world;

--- templates/default/test/{%=name%}.js
+++ templates/default/test/{%=name%}.js
@@ -6,7 +6,7 @@ var {%= js_name %} = require('{%= main_in_test %}');
 var assert = require('assert');
 
 describe("description", function(){
-  it("should has a method `init`", function(){
-    assert.ok('init' in {%= js_name %});
+  it("should do blah blah blah", function(){
+    // your code...
   });
 });
